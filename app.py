9f731d3507454fd8323b244692f482bc91a7ff3c
Watty_Sylvester <88721196+masindes@users.noreply.github.com>
2025-03-22 21:46:50 +0300

    database

--- app.py
+++ app.py
@@ -11,6 +11,11 @@ basedir = os.path.dirname(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///' + os.path.join(basedir, 'db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False    
 
+#nitialze database
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+
 #Run server
 if __name__ == '__main__':
     app.run(debug=True)
