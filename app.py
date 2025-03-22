e8f702270cda0fef1e296c337dc1ffa9af13dec4
Watty_Sylvester <88721196+masindes@users.noreply.github.com>
2025-03-22 21:32:48 +0300

    server

--- app.py
+++ app.py
@@ -2,3 +2,10 @@ from flask import Flask,request,jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
+#initialize application
+app = Flask(__name__)
+
+
+#Run server
+if __name__ == '__main__':
+    app.run(debug=True)
