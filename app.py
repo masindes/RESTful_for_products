8741318ac191557f877d510387a00e84d1cedef1
Watty_Sylvester <88721196+masindes@users.noreply.github.com>
2025-03-22 21:44:35 +0300

    database

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 
 basedir = os.path.dirname(os.path.dirname(__file__))
 
+#Database
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False    
 
 #Run server
 if __name__ == '__main__':
